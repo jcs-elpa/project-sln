df8ea1e8c217d3e16cd9d6938c4de28187d2f2ed
jenchieh <jcs090218@gmail.com>
2019-11-11 02:14:36 +0800

    Add core.

---
+++ project-sln.el
@@ -0,0 +1,38 @@
+;;; project-sln.el --- Project structure organizer.  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Shen, Jen-Chieh
+;; Created date 2019-11-10 21:03:32
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Project structure organizer.
+;; Keyword: project structure organize
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/jcs090218/project-sln
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Project structure organizer.
+;;
+
+;;; Code:
+
+
+
+(provide 'project-sln)
+;;; project-sln.el ends here
