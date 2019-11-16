9629231a5985674ba898e0212a714b8e7b19ddba
jenchieh <jcs090218@gmail.com>
2019-11-16 20:38:35 +0800

    Add load parse-it.

---
+++ scripts/load-parse-it.el
@@ -0,0 +1,34 @@
+
+(require 'f)
+
+(defun parse-it-test--load-files (dir files)
+  "Load FILES under a DIR."
+  (dolist (path files)
+    (let ((filepath (concat (file-name-as-directory dir) path)))
+      (when (and
+             ;; ignore `-pkg' file.
+             (not (string-match-p "-pkg.el" filepath))
+             ;; Ignore test files.
+             (not (string-match-p "test" filepath)))
+        (load-file filepath)))))
+
+(defun parse-it-test--all-dirs (lst)
+  "Load LST of directory."
+  (dolist (dir lst)
+    (let ((files-el (directory-files dir nil "\\.el$"))
+          (files-elc (directory-files dir nil "\\.elc$")))
+      (parse-it-test--load-files dir files-el)
+      (parse-it-test--load-files dir files-elc))))
+
+;; --------------------------------------------------------------------------
+;; Start testing..
+
+(let* ((project-path (expand-file-name "../../parse-it/"))
+       (dirs (f-directories project-path))
+       (final-list (append (list project-path) dirs)))
+  (dolist (path final-list) (add-to-list 'load-path path))
+  (parse-it-test--all-dirs final-list)  ; Load all core files.
+
+  (require 'parse-it)
+
+  (message "[INFO] Done require 'parse-it' ::\n"))
